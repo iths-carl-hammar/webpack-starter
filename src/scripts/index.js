d367f5aed438119f4a02157b6e356ee105157b15
kalle <carl.hammar@gmail.com>
2019-10-24 14:53:23 +0200

    index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,20 @@
 import '../styles/index.scss';
+import anime from 'animejs';
 
 console.log('webpack starterkit');
+
+
+anime({
+    targets:'div.box1',
+    translateY:[
+        {value:200, duration:500},
+        {value:0, duration:800}
+    ],
+    rotate:{
+        value:'1turn',
+        easing:'easeInOutSine'
+    }
+});
+
+
+
