48a0315c880bf7a8030a49d1246c8ee6407b3a77
kalle <carl.hammar@gmail.com>
2019-10-25 14:37:02 +0200

    skiss och allt från gammalt

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,8 +5,8 @@ console.log('webpack starterkit');
 
 
 anime({
-    targets:'div.box1',
-    translateY:[
+    targets:'',
+    translateX:[
         {value:200, duration:500},
         {value:0, duration:800}
     ],
@@ -15,6 +15,22 @@ anime({
         easing:'easeInOutSine'
     }
 });
+
+function toggleMenu(){
+    let element = document.querySelector("nav"); // hämntar in html element = tagen nav . 
+    element.classList.toggle("menu-hidden"); // hämntar in class name på nav tag och läger på en method = toggle.med variabel element.och CLASS list som är Domtoken.
+
+    element.document.querySelector(".fade-layer");
+    element.classList.toggle("hidden");
+
+
+}
+let element = document.querySelector("#menu-btn"); // hämntar in hamburgare meny .
+element.addEventListener("click", toggleMenu ); // sätter på click event på functionen toggleMenu. 
+element = document.querySelector(".fade-layer");  // använd = annars underfind. 
+element.addEventListener("click", toggleMenu);//  method. 
+
+
 
 
 
